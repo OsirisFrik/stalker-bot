422f6b4c4c3c07a6566545c758d917eba0333610
osirisfrik <osiris.lpz96@gmail.com>
2019-11-25 17:00:33 -0600

    debug env

--- firebase.js
+++ firebase.js
@@ -1,5 +1,9 @@
 const firebase = require('firebase-admin')
 
+console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+console.log('file')
+console.log(require(process.env.GOOGLE_APPLICATION_CREDENTIALS))
+
 firebase.initializeApp({
   credential: firebase.credential.applicationDefault(),
   databaseURL: process.env.DATABASE,
