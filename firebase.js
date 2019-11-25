b8aff0a3597ef1f564d1c79c2b1993ef3a2c6dde
osirisfrik <osiris.lpz96@gmail.com>
2019-11-25 17:14:47 -0600

    remove vscode settings

--- firebase.js
+++ firebase.js
@@ -1,9 +1,5 @@
 const firebase = require('firebase-admin')
 
-console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS)
-console.log('file')
-console.log(require(process.env.GOOGLE_APPLICATION_CREDENTIALS))
-
 firebase.initializeApp({
   credential: firebase.credential.applicationDefault(),
   databaseURL: process.env.DATABASE,
